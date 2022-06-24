a35d61cb5e2065041470118b8b9f0396ddacc79b
mgamboa <gamboa.martin@gmail.com>
2022-06-24 17:41:09 -0500

    v0.4.0 Se integran etiquetas

---
+++ template/etiquetas/_footer_lista.php
@@ -0,0 +1,5 @@
+<?php /** @var base\controller\controlador_base $controlador */ ?>
+<div class="box-body">
+    * Total registros: <?php echo $controlador->n_registros; ?><br />
+    * Fecha Hora: <?php echo $controlador->fecha_hoy; ?>
+</div>
---
+++ template/etiquetas/_titulo_lista.php
@@ -0,0 +1,4 @@
+<?php /** @var base\controller\controler $controlador */ ?>
+<div class="widget-header text-uppercase">
+    <h2><?php echo $controlador->titulo_lista; ?></h2>
+</div>
