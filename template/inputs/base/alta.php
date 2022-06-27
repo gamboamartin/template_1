5cd5c04609cb7af35553b3bad661a6a71abb566d
mgamboa <gamboa.martin@gmail.com>
2022-06-27 12:14:52 -0500

    v0.21.0 form

---
+++ template/inputs/base/alta.php
@@ -0,0 +1,7 @@
+<?php use config\views; ?>
+<?php include (new views())->ruta_templates."inputs/alta/codigo.php"; ?>
+<?php include (new views())->ruta_templates."inputs/alta/codigo_bis.php"; ?>
+<?php include (new views())->ruta_templates."inputs/alta/descripcion.php"; ?>
+<?php include (new views())->ruta_templates."inputs/alta/alias.php"; ?>
+<?php include (new views())->ruta_templates."inputs/alta/descripcion_select.php"; ?>
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
---
+++ template/inputs/base/modifica.php
@@ -0,0 +1,9 @@
+<?php use config\views; ?>
+<?php include (new views())->ruta_templates."inputs/modifica/id.php"; ?>
+<?php include (new views())->ruta_templates."inputs/modifica/codigo.php"; ?>
+<?php include (new views())->ruta_templates."inputs/modifica/codigo_bis.php"; ?>
+<?php include (new views())->ruta_templates."inputs/modifica/descripcion.php"; ?>
+<?php include (new views())->ruta_templates."inputs/modifica/alias.php"; ?>
+<?php include (new views())->ruta_templates."inputs/modifica/descripcion_select.php"; ?>
+<?php include (new views())->ruta_templates."botons/status.php"; ?>
+<?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
