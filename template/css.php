784bdbe8880d2a5fbe58123a280eb6d1cf9e3e84
mgamboa <gamboa.martin@gmail.com>
2022-06-27 10:16:52 -0500

    v0..0 Se integran java y css

---
+++ template/css.php
@@ -0,0 +1,31 @@
+<?php use config\generales;
+use config\views; ?>
+<link rel="icon" href="<?php echo (new generales())->url_base; ?>img/icons/Icono-TIQUE.png" type="image/x-icon" />
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,800,700&amp;subset=latin,cyrillic' rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/font-awesome/css/font-awesome.min.css" />
+<!-- Start BOOTSTRAP -->
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/tether/dist/css/tether.min.css" />
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/bootstrap/dist/css/bootstrap.min.css" />
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/bootstrap-select.min.css" />
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/bootstrap-colorpicker-master/dist/css/bootstrap-colorpicker.min.css" />
+<!-- End Bootstrap -->
+<!-- Start Template files -->
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/winter-flat.css" />
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/custom.css" />
+<!-- End  Template files -->
+<!-- Start owl-carousel -->
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/owl.carousel/assets/owl.carousel.css" />
+<!-- End owl-carousel -->
+<!-- Start JS MAP  -->
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/map.css" />
+<!-- End JS MAP  -->
+<!-- Start blueimp  -->
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/blueimp-gallery.min.css" />
+<!-- End blueimp  -->
+<!-- Start custom template style  -->
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/custom_template_style.css" />
+<!-- End custom template style   -->
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/footable-jquery/css/footable.bootstrap.min.css" />
+<!-- Inicio template proyecto local  -->
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/proyecto.css" />
+<!-- End template proyecto local   -->
---
+++ template/java.php
@@ -0,0 +1,42 @@
+<?php use config\generales;
+use config\views; ?>
+
+<!-- Start Jquery -->
+<script src="<?php echo (new views())->url_assets; ?>js/jquery-2.2.1.min.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>libraries/jquery.mobile/jquery.mobile.custom.min.js"></script>
+<!-- End Jquery -->
+<!-- Start BOOTSTRAP -->
+<script src="<?php echo (new views())->url_assets; ?>libraries/tether/dist/js/tether.min.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>libraries/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>js/bootstrap-select.min.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>libraries/bootstrap-colorpicker-master/dist/js/bootstrap-colorpicker.min.js"></script>
+<!-- End Bootstrap -->
+<!-- Start Template files -->
+<script src="<?php echo (new views())->url_assets; ?>js/winter-flat.js"></script>
+<!-- End  Template files -->
+<!-- Start owl-carousel -->
+<script src="<?php echo (new views())->url_assets; ?>libraries/owl.carousel/owl.carousel.min.js"></script>
+<!-- End owl-carousel -->
+<!-- Start JS MAP  -->
+<script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyABFoveisZMRWysqm4eCzhf_Jx2Yka4jEg&libraries=weather,geometry,visualization,places,drawing" type="text/javascript">
+</script>
+<script type="text/javascript" src="<?php echo (new views())->url_assets; ?>js/map_infobox.js"></script>
+<script type="text/javascript" src="<?php echo (new views())->url_assets; ?>js/markerclusterer.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>js/map.js" type="text/javascript"></script>
+<!-- End JS MAP  -->
+<!-- Start blueimp  -->
+<script src="<?php echo (new views())->url_assets; ?>js/blueimp-gallery.min.js" type="text/javascript"></script>
+<!-- End blueimp  -->
+<script src="<?php echo (new views())->url_assets; ?>js/modernizr.custom.js"></script>
+<!-- Start custom template style  -->
+<script src="<?php echo (new views())->url_assets; ?>js/custom_template_style.js" type="text/javascript"></script>
+<!-- End custom template style   -->
+
+<script src="<?php echo (new views())->url_assets; ?>libraries/footable-jquery/js/footable.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>js/footable_init.js"></script>
+
+<script src="<?php echo (new views())->url_assets; ?>js/dpejes/dpe.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>js/dpejes_custom.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>js/dpejes_custom.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>js/facebook.js" type="text/javascript"></script>
