39f24d1e1b63b52690c440fc8449cc234b3b8ac0
mgamboa <gamboa.martin@gmail.com>
2022-06-27 12:33:50 -0500

    v0.25.0 actions

---
+++ template/listas/action_row.php
@@ -0,0 +1,3 @@
+<?php use config\views; ?>
+<td><?php include (new views())->ruta_templates.'botons/base/modifica.php';?></td>
+<td><?php include (new views())->ruta_templates.'botons/base/elimina_bd.php';?></td>
