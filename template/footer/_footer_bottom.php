5e2cf9df376ae390a85bcfc834ef920e4d7304f4
mgamboa <gamboa.martin@gmail.com>
2022-06-27 11:22:49 -0500

    v0.13.0 Footer

---
+++ template/footer/_footer_bottom.php
@@ -0,0 +1,5 @@
+<div class="footer-bottom"> <!--link-->
+    <div class="container text-right">
+        <span class=""><a href="#">WINTER &#169; 2016</a></span>
+    </div>
+</div><!-- /.footer-bottom -->
---
+++ template/footer/_icono.php
@@ -0,0 +1,2 @@
+<?php use config\views; ?>
+<div class="logo"><a href="#"><img src="<?php echo (new views())->url_assets; ?>img/icons/Icono-TIQUE.png" alt="" /></a></div>
