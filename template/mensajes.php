5a078be1a8e859d56f2e05083594425d58f0f2cb
mgamboa <gamboa.martin@gmail.com>
2022-06-27 11:57:36 -0500

    v0.17.0 mensajes

---
+++ template/mensajes.php
@@ -0,0 +1,3 @@
+<?php /** @var controllers\base\system $controlador  viene de registros del controler/lista */ ?>
+<?php  echo $controlador->mensaje_exito; ?>
+<?php  echo $controlador->mensaje_warning; ?>
