f91c7eb03864456aac793aa8527eb68f1197cd68
mgamboa <gamboa.martin@gmail.com>
2022-06-27 11:28:46 -0500

    v0.14.0 Footer

---
+++ template/footer/_footer.php
@@ -0,0 +1,9 @@
+<?php use config\views; ?>
+<div class="container footer-mask">
+    <div class="container footer-contant">
+        <div class="row">
+            <div class="col-md-3 hidden-sm hidden-xs"><!--Div img--></div>
+        </div>
+    </div><!-- /.footer-content -->
+    <?php include (new views())->ruta_templates.'footer/_footer_bottom.php'?>
+</div>
