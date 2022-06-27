774ff7c54eaadb229afc0e6f4b1d2e54cbf61c96
mgamboa <gamboa.martin@gmail.com>
2022-06-27 11:46:28 -0500

    v0.15.0 breadcrumbs

---
+++ template/breadcrumb/alta.php
@@ -0,0 +1,2 @@
+<?php /** @var base\controller\controler $controlador */ ?>
+<li class="item"><a href="<?php  echo $controlador->link_alta; ?>"> Alta </a></li>
---
+++ template/breadcrumb/lista.php
@@ -0,0 +1,2 @@
+<?php /** @var base\controller\controler $controlador */ ?>
+<li class="item"><a href="<?php  echo $controlador->link_lista; ?>"> Lista </a></li>
