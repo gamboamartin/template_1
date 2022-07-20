e62defe0f3af0dcb59a277ed5272b907cec2c33f
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-20 14:58:16 -0500

    v.0.61.7 Se ajusta codigo en directivas

--- src/directivas.php
+++ src/directivas.php
@@ -11,7 +11,12 @@ class directivas extends \gamboamartin\template\directivas {
 
     }
 
-
+    /**
+     * Funcion que genera boton valida
+     * @param int $registro_id
+     * @param string $valida_persona_fisica
+     * @return array|string
+     */
     public function button_href_valida_persona_fisica(int $registro_id, string $valida_persona_fisica): array|string
     {
 
@@ -117,35 +122,7 @@ class directivas extends \gamboamartin\template\directivas {
         return $div;
     }
 
-    /**
-     * Genera un input de tipo codigo
-     * @param int $cols Numero de columnas boostrap
-     * @version 0.32.4
-     * @param stdClass $row_upd Registro obtenido para actualizar
-     * @param bool $value_vacio Para altas en caso de que sea vacio o no existe el key
-     * @return array|string
-     */
-    public function input_codigo(int $cols, stdClass $row_upd, bool $value_vacio): array|string
-    {
 
-        $valida = $this->valida_cols(cols: $cols);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
-        }
-
-        $html =$this->input_text_required(disable: false,name: 'codigo',place_holder: 'Codigo',row_upd: $row_upd,
-            value_vacio: $value_vacio);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar input', data: $html);
-        }
-
-        $div = $this->html->div_group(cols: $cols,html:  $html);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
-        }
-
-        return $div;
-    }
 
     /**
      * Genera un input de tipo codigo bis
@@ -281,41 +258,7 @@ class directivas extends \gamboamartin\template\directivas {
 
     }
 
-    /**
-     * Genera un input tipo required
-     * @version 0.32.2
-     * @param stdClass $row_upd Registro obtenido para actualizar
-     * @param bool $disable si disabled retorna el input como disabled
-     * @param string $name Usado para identificador css name input y place holder
-     * @param string $place_holder Texto a mostrar en el input
-     * @param bool $value_vacio Para altas en caso de que sea vacio o no existe el key
-     * @return array|string
-     */
-    public function input_text_required(bool $disable, string $name, string $place_holder, stdClass $row_upd,
-                                         bool $value_vacio ): array|string
-    {
 
-        $valida = $this->valida_data_label(name: $name,place_holder:  $place_holder);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al validar datos ', data: $valida);
-        }
-
-        $init = $this->init_text(name: $name,place_holder:  $place_holder, row_upd: $row_upd,value_vacio:  $value_vacio);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al inicializar datos', data: $init);
-        }
-
-        $html= $this->html->text(disabled:$disable, id_css: $name, name: $name, place_holder: $place_holder,
-            required: true, value: $init->row_upd->$name);
-
-        $div = $this->html->div_label(html:  $html,label:$init->label);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
-        }
-
-        return $div;
-
-    }
 
 
     /**
