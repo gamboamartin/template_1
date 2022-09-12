eb970b617f27dc15932a8cf4bd65643e88fb1734
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-09-12 16:49:10 -0500

    v.0.74.17 Implementacion template breadcrubs

--- views/basics/lista.php
+++ views/basics/lista.php
@@ -1,9 +1,12 @@
+<?php /** @var gamboamartin\system\ $controlador  viene de registros del controler/lista */ ?>
 <?php use config\views; ?>
 <main class="main section-color-primary">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <?php include (new views())->ruta_templates."head/lista/title.php"; ?>
+                <?php if($controlador->include_breadcrumb!==''){
+                    include $controlador->include_breadcrumb;
+                } ?>
                 <?php include (new views())->ruta_templates."mensajes.php"; ?>
                 <?php include (new views())->ruta_templates.'listas/content.php';?>
             </div><!-- /.center-content -->
