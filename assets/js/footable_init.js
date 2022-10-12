cb98954e12aa49068924b53c16a3ba56678c7aa7
Kevin <kevin.acuna@ivitec.com.mx>
2022-10-12 09:11:31 -0500

    v0.75.17 Se integra paginacion

--- assets/js/footable_init.js
+++ assets/js/footable_init.js
@@ -14,4 +14,14 @@ function footable_init() {
         "enabled": true
               },
     });
+
+    $('.footable-filtering .form-inline')
+        .prepend($('<div class="form-group">')
+            .append($('<select class="footable-filter-size form-control"></select>')
+                .append('<option>10</option><option>25</option><option>50</option><option>100</option>')))
+
+    $('.footable-filter-size').on('change', function(e){
+        let size = $(this).val();
+        FooTable.get('#footable').pageSize(parseInt(size));
+    });
 }
--- template/listas/table.php
+++ template/listas/table.php
@@ -1,6 +1,6 @@
 <?php use config\views; ?>
 <?php /** @var base\controller\ $controlador  viene de registros del controler/lista */ ?>
-<table class="table table-striped footable-sort" data-sorting="true">
+<table id="footable" class="table table-striped footable-sort" data-paging="true" data-filtering="true" data-paging-position="right">
     <?php include $controlador->include_lista_thead;?>
     <?php include (new views())->ruta_templates.'listas/rows.php';?>
 </table>
