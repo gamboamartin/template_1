e15e8cb1ab93758cc0422d68885707d5be10317c
mgamboa <gamboa.martin@gmail.com>
2022-10-19 09:20:39 -0500

    v0.83.18 Seimplementa table responsive

--- template/listas/content.php
+++ template/listas/content.php
@@ -1,7 +1,7 @@
 <?php use config\views; ?>
-<div class="widget widget-box box-container widget-mylistings">
+<div class="widget widget-box box-container">
     <?php include (new views())->ruta_templates.'etiquetas/_titulo_lista.php';?>
-    <div class="">
+    <div class="table-responsive">
         <?php include (new views())->ruta_templates.'listas/table.php';?>
         <?php include (new views())->ruta_templates.'etiquetas/_footer_lista.php';?>
     </div>
