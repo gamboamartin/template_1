4bc9c5982f1f70db687144ce741e47c1f795cf1d
mgamboa <gamboa.martin@gmail.com>
2022-11-01 12:16:10 -0600

    v0.93.18 FIX salidar error nav menu

--- template/nav/menu.php
+++ template/nav/menu.php
@@ -14,7 +14,15 @@ use gamboamartin\template_1\nav;
         <!-- Links -->
         <div class="collapse navbar-collapse" id="main-menu">
             <ul class="nav navbar-nav clearfix">
-                <?php echo (new nav())->lis_menu_principal(links: $links_menu, secciones: $controlador->secciones_permitidas); ?>
+                <?php $nav =  (new nav())->lis_menu_principal(links: $links_menu, secciones: $controlador->secciones_permitidas); ?>
+                <?php
+                if(\gamboamartin\errores\errores::$error){
+                    $error = (new \gamboamartin\errores\errores())->error(mensaje: 'Error al generar nav', data: $nav);
+                    print_r($error);
+                    exit;
+                }
+                ?>
+                <?php echo $nav; ?>
             </ul>
         </div>
     </nav>
