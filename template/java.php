201b7b497a4f4660ba93489a89a4987dd4902d5a
Kevin <kevin.acuna@ivitec.com.mx>
2023-02-08 11:20:52 -0500

    v8.6.0 Se integra select datatables

--- template/java.php
+++ template/java.php
@@ -40,6 +40,7 @@ use config\views; ?>
 <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.6.0/js/dataTables.select.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/imask/6.4.3/imask.min.js"
         integrity="sha512-yJ3vm1HmQtcgeMtbUYCp7PuTLyjU+ffCnVNTuE1Uccv1BmkoaJIXt1EjBVGnscjCILc62hTJJJ2rJJBTcw8RjQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
