f25a1910ef8193520255cf4badb6038064d10b6f
mgamboa <gamboa.martin@gmail.com>
2023-03-07 15:37:39 -0600

    v8.18.0 Se integran parents ir

--- template/forms/modifica.php
+++ template/forms/modifica.php
@@ -27,6 +27,15 @@
 
 <div class="col-md-12 buttons-form">
     <?php
+    foreach ($controlador->buttons_parents_ir as $button){ ?>
+        <div class="col-md-4">
+            <?php echo $button; ?>
+        </div>
+    <?php } ?>
+</div>
+
+<div class="col-md-12 buttons-form">
+    <?php
     foreach ($controlador->buttons_parents_alta as $button){ ?>
         <div class="col-md-4">
             <?php echo $button; ?>
