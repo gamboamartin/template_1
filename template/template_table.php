cb5d237acaa08be2d237ea370742e9b227531c3f
Kevin <kevin.acuna@ivitec.com.mx>
2023-04-03 18:26:15 -0500

    v9.2.0 Se integra estructura template

--- template/template_table.php
+++ template/template_table.php
@@ -1,1 +1,62 @@
-<h2>Cabecera</h2>
+<link rel="stylesheet" href="icons.css">
+
+<link rel="stylesheet" href="owl.carousel.min.css">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href=bootstrap.min.css">
+
+<!-- Style -->
+<link rel="stylesheet" href=style.css">
+
+<div class="content">
+
+    <div class="container">
+        <h2 class="mb-5">Table #4</h2>
+
+        <div class="table-responsive custom-table-responsive">
+
+            <table class="table custom-table">
+                <thead>
+                <tr>
+
+                    <th scope="col">
+                        <label class="control control--checkbox">
+                            <input type="checkbox" class="js-check-all"/>
+                            <div class="control__indicator"></div>
+                        </label>
+                    </th>
+
+                    <?php foreach ($this->datatable['columns'] as $column): ?>
+                        <th scope="col"><?php print_r($column->title); ?></th>
+                    <?php endforeach; ?>
+
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($registros as $registro): ?>
+                    <tr scope="row">
+                        <th scope="row">
+                            <label class="control control--checkbox">
+                                <input type="checkbox"/>
+                                <div class="control__indicator"></div>
+                            </label>
+                        </th>
+                        <?php foreach ($this->datatable['columns'] as $column): ?>
+                            <th scope="col"><?php echo $registro[$column->data]; ?></th>
+                        <?php endforeach; ?>
+                    </tr>
+                    <tr class="spacer">
+                        <td colspan="100"></td>
+                    </tr>
+                <?php endforeach; ?>
+
+
+
+                </tbody>
+            </table>
+        </div>
+
+
+    </div>
+
+</div>
