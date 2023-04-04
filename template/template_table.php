9c132ee027b765b92beea3c325faba14485f73da
Kevin <kevin.acuna@ivitec.com.mx>
2023-04-04 12:19:24 -0500

    v9.3.0 Se ajusta definicion de parametros para template

--- template/template_table.php
+++ template/template_table.php
@@ -1,62 +1,38 @@
-<link rel="stylesheet" href="icons.css">
+<div class="table-responsive custom-table-responsive">
 
-<link rel="stylesheet" href="owl.carousel.min.css">
+    <table class="table custom-table">
+        <thead>
+        <tr>
+            <th scope="col">
+                <label class="control control--checkbox">
+                    <input type="checkbox" class="js-check-all"/>
+                    <div class="control__indicator"></div>
+                </label>
+            </th>
 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href=bootstrap.min.css">
+            <?php foreach ($this->datatable['columns'] as $column): ?>
+                <th scope="col" style="vertical-align: middle;"><?php print_r($column->title); ?></th>
+            <?php endforeach; ?>
+        </tr>
+        </thead>
+        <tbody>
 
-<!-- Style -->
-<link rel="stylesheet" href=style.css">
-
-<div class="content">
-
-    <div class="container">
-        <h2 class="mb-5">Table #4</h2>
-
-        <div class="table-responsive custom-table-responsive">
-
-            <table class="table custom-table">
-                <thead>
-                <tr>
-
-                    <th scope="col">
-                        <label class="control control--checkbox">
-                            <input type="checkbox" class="js-check-all"/>
-                            <div class="control__indicator"></div>
-                        </label>
-                    </th>
-
-                    <?php foreach ($this->datatable['columns'] as $column): ?>
-                        <th scope="col"><?php print_r($column->title); ?></th>
-                    <?php endforeach; ?>
-
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($registros as $registro): ?>
-                    <tr scope="row">
-                        <th scope="row">
-                            <label class="control control--checkbox">
-                                <input type="checkbox"/>
-                                <div class="control__indicator"></div>
-                            </label>
-                        </th>
-                        <?php foreach ($this->datatable['columns'] as $column): ?>
-                            <th scope="col"><?php echo $registro[$column->data]; ?></th>
-                        <?php endforeach; ?>
-                    </tr>
-                    <tr class="spacer">
-                        <td colspan="100"></td>
-                    </tr>
+        <?php foreach ($registros as $registro): ?>
+            <tr scope="row">
+                <th scope="row">
+                    <label class="control control--checkbox">
+                        <input type="checkbox"/>
+                        <div class="control__indicator"></div>
+                    </label>
+                </th>
+                <?php foreach ($this->datatable['columns'] as $column): ?>
+                    <td scope="col"><?php echo $registro[$column->data]; ?></td>
                 <?php endforeach; ?>
-
-
-
-                </tbody>
-            </table>
-        </div>
-
-
-    </div>
-
+            </tr>
+            <tr class="spacer">
+                <td colspan="100"></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
 </div>
