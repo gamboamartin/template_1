4601ffc22555c6b3bec361a68098fd337a005787
Kevin <kevin.acuna@ivitec.com.mx>
2023-04-10 13:13:07 -0500

    v9.6.0 Se ajusta disposicion de registros

--- template/template_table.php
+++ template/template_table.php
@@ -1,6 +1,22 @@
 <div class="table-responsive custom-table-responsive" style="margin-bottom: 100px;">
 
-    <table class="table custom-table">
+    <div class="table-details" style="display: flex; justify-content: space-between;align-items: center;">
+        <div>Mostrando 1 - 5 de 20 entradas</div>
+        <div class="fg-line" style="position: relative;">
+            <i class="bi bi-search" style="position: absolute;
+top: 10px;
+left: 12px;"></i>
+            <input type="search" placeholder="Buscar registro..." id="search_id-search-input" name="s" value="" style="background: #FFFFFF;border: 1px solid #DBDFEA;
+    box-sizing: border-box;
+    box-shadow: 0 0.5px 2px rgba(15, 30, 81, 0.11);
+    border-radius: 4px;
+    margin: 0;
+    padding: 8px 0 8px 30px;
+    min-width: 300px;">
+        </div>
+    </div>
+
+    <table class="table custom-table caption-top">
         <thead>
         <tr>
             <th scope="col">
@@ -31,13 +47,14 @@
                 <?php endforeach; ?>
                 <td align="center">
                     <div class="dropdown">
-                        <a class="btn btn-secondary" href="#" role="button" id="dropdownMenuLink4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="btn btn-secondary" href="#" role="button" id="dropdownMenuLink4"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="bi bi-three-dots-vertical"></i>
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <div class="dropdown-menu pull-right" aria-labelledby="dropdownMenuButton">
                             <?php foreach ($registros['acciones'] as $accion): ?>
                                 <a class="dropdown-item"
-                                   href="./index.php?seccion=<?php echo $accion['adm_seccion_descripcion']; ?>&accion=<?php echo $accion['adm_accion_descripcion']; ?>&registro_id=<?php echo $registro[$this->tabla."_id"]; ?>&session_id=<?php echo $this->session_id; ?>"><?php echo $accion['adm_accion_titulo']; ?></a>
+                                   href="./index.php?seccion=<?php echo $accion['adm_seccion_descripcion']; ?>&accion=<?php echo $accion['adm_accion_descripcion']; ?>&registro_id=<?php echo $registro[$this->tabla . "_id"]; ?>&session_id=<?php echo $this->session_id; ?>"><?php echo $accion['adm_accion_titulo']; ?></a>
                             <?php endforeach; ?>
                         </div>
                     </div>
