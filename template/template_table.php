cb10638eff16a2101d341f6989ba01e5d4859980
Kevin <kevin.acuna@ivitec.com.mx>
2023-04-10 11:53:50 -0500

    v9.5.0 Se integra template append para lectura

--- template/template_table.php
+++ template/template_table.php
@@ -1,4 +1,4 @@
-<div class="table-responsive custom-table-responsive">
+<div class="table-responsive custom-table-responsive" style="margin-bottom: 100px;">
 
     <table class="table custom-table">
         <thead>
@@ -13,11 +13,12 @@
             <?php foreach ($this->datatable['columns'] as $column): ?>
                 <th scope="col" style="vertical-align: middle;"><?php print_r($column['titulo']); ?></th>
             <?php endforeach; ?>
+            <th scope="col"></th>
         </tr>
         </thead>
         <tbody>
 
-        <?php foreach ($registros as $registro): ?>
+        <?php foreach ($registros['data'] as $registro): ?>
             <tr scope="row">
                 <th scope="row">
                     <label class="control control--checkbox">
@@ -28,6 +29,19 @@
                 <?php foreach ($this->datatable['columns'] as $key => $column): ?>
                     <td scope="col"><?php echo $registro[$key]; ?></td>
                 <?php endforeach; ?>
+                <td align="center">
+                    <div class="dropdown">
+                        <a class="btn btn-secondary" href="#" role="button" id="dropdownMenuLink4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="bi bi-three-dots-vertical"></i>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <?php foreach ($registros['acciones'] as $accion): ?>
+                                <a class="dropdown-item"
+                                   href="./index.php?seccion=<?php echo $accion['adm_seccion_descripcion']; ?>&accion=<?php echo $accion['adm_accion_descripcion']; ?>&registro_id=<?php echo $registro[$this->tabla."_id"]; ?>&session_id=<?php echo $this->session_id; ?>"><?php echo $accion['adm_accion_titulo']; ?></a>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                </td>
             </tr>
             <tr class="spacer">
                 <td colspan="100"></td>
---
+++ template/template_table_append.php
@@ -0,0 +1,29 @@
+<?php foreach ($registros['data'] as $registro): ?>
+    <tr scope="row">
+        <th scope="row">
+            <label class="control control--checkbox">
+                <input type="checkbox"/>
+                <div class="control__indicator"></div>
+            </label>
+        </th>
+        <?php foreach ($this->datatable['columns'] as $key => $column): ?>
+            <td scope="col"><?php echo $registro[$key]; ?></td>
+        <?php endforeach; ?>
+        <td align="center">
+            <div class="dropdown">
+                <a class="btn btn-secondary" href="#" role="button" id="dropdownMenuLink4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="bi bi-three-dots-vertical"></i>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <?php foreach ($registros['acciones'] as $accion): ?>
+                        <a class="dropdown-item"
+                           href="./index.php?seccion=<?php echo $accion['adm_seccion_descripcion']; ?>&accion=<?php echo $accion['adm_accion_descripcion']; ?>&registro_id=<?php echo $registro[$this->tabla."_id"]; ?>&session_id=<?php echo $this->session_id; ?>"><?php echo $accion['adm_accion_titulo']; ?></a>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </td>
+    </tr>
+    <tr class="spacer">
+        <td colspan="100"></td>
+    </tr>
+<?php endforeach; ?>
