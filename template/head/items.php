91eeef8506ef40ae24ca96bdfb24bc874e6fa76e
mgamboa <gamboa.martin@gmail.com>
2023-07-05 11:58:51 -0600

    v9.21.1 Se integran buttons full

--- template/head/items.php
+++ template/head/items.php
@@ -4,5 +4,5 @@ use gamboamartin\system\links_menu; ?>
     <?php include (new views())->ruta_templates."breadcrumb/adm_session/inicio.php"; ?>
     <?php include (new views())->ruta_templates."breadcrumb/alta.php"; ?>
     <li class="item"><a role="button" class="btn btn-warning item-br" href="<?php  echo $controlador->link_lista; ?>"> Lista </a></li>
-    <li class="item"><a role="button" class="btn btn-danger item-br" href="<?php echo (new links_menu(link: $controlador->link,registro_id: $controlador->registro_id))->links->adm_session->logout ;?>"> Salir </a></li>
+
 </ul>
--- views/adm_session/inicio.php
+++ views/adm_session/inicio.php
@@ -9,7 +9,7 @@ use gamboamartin\system\links_menu; ?>
             <div class="top-title">
                 <ul class="breadcrumb">
                     <?php include (new views())->ruta_templates."breadcrumb/adm_session/inicio.php"; ?>
-                    <li class="item"><a role="button" class="btn btn-danger item-br" href="<?php echo (new links_menu(link: $controlador->link,registro_id: $controlador->registro_id))->links->adm_session->logout ;?>"> Salir </a></li>
+
                 </ul>
                 <h1 class="h-side-title page-title page-title-big text-color-primary">
                     Bienvenido a <?php echo (new views())->titulo_sistema ;?>
