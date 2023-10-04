d7c72f9f9dd04a244115102654f5fe335d315ecd
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-10-04 16:27:08 -0600

    v14.1.0 Se integra elemento button descarga

--- template/head/items.php
+++ template/head/items.php
@@ -4,5 +4,6 @@ use gamboamartin\system\links_menu; ?>
     <?php include (new views())->ruta_templates."breadcrumb/adm_session/inicio.php"; ?>
     <?php include (new views())->ruta_templates."breadcrumb/alta.php"; ?>
     <li class="item"><a role="button" class="btn btn-warning item-br" href="<?php  echo $controlador->link_lista; ?>"> Lista </a></li>
+    <li class="item"><a role="button" class="btn btn-warning item-br descarga_excel" href="<?php  echo $controlador->link_descarga_excel; ?>"> Descarga Excel </a></li>
 
 </ul>
