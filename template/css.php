fff3f8fde3dc52a61cb9280493ef7bdf3a34dae3
mgamboa <gamboa.martin@gmail.com>
2024-03-06 09:36:05 -0600

    v20.0.0 Se integra icons v 11

--- template/css.php
+++ template/css.php
@@ -34,4 +34,4 @@ use config\views; ?>
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css" />
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css" />
 <link rel="stylesheet" href="https://cdn.datatables.net/select/1.6.0/css/select.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
