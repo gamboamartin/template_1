4302d2e7e02b4af6f0da6519738cfd4a60bc9abf
mgamboa <gamboa.martin@gmail.com>
2024-08-07 11:33:20 -0600

    v24.7.0 se integra cerrar session en menun head

--- template/nav/_redes_sociales.php
+++ template/nav/_redes_sociales.php
@@ -10,13 +10,14 @@ use gamboamartin\system\links_menu;
         <ul class="social-nav clearfix">
             <?php echo $controlador->menu_header; ?>
         </ul>
+        <?php
+        if(isset($_SESSION['activa']) && (int)$_SESSION['activa'] === 1){ ?>
+            <div class="pull-right col-md-12" style="text-align: right;">
+                <a role="button" class="btn btn-danger cerrar-session" href="<?php echo (new links_menu(link: $controlador->link,registro_id: $controlador->registro_id))->links->adm_session->logout ;?>"> Salir </a>
+            </div>
+        <?php }
+        ?>
     </div>
-    <?php
-    if(isset($_SESSION['activa']) && (int)$_SESSION['activa'] === 1){ ?>
-        <div class="pull-right col-md-12" style="text-align: right;">
-            <a role="button" class="btn btn-danger cerrar-session" href="<?php echo (new links_menu(link: $controlador->link,registro_id: $controlador->registro_id))->links->adm_session->logout ;?>"> Salir </a>
-        </div>
-    <?php }
-    ?>
+
 </div>
 </div>
