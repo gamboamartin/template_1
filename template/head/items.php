cdbbed1a4033030c9b42982b7d3b54ccdd632916
mgamboa <gamboa.martin@gmail.com>
2024-08-07 11:46:13 -0600

    v24.8.0 se integra cerrar session en menun head

--- template/head/items.php
+++ template/head/items.php
@@ -2,9 +2,8 @@
 /**
  * @var base\controller\controlador_base $controlador
  */
-use gamboamartin\system\links_menu; ?>
+?>
 <ul class="breadcrumb">
-    <?php include (new views())->ruta_templates."breadcrumb/adm_session/inicio.php"; ?>
     <?php
     foreach ($controlador->acciones_visibles_permitidas as $accion_visible) {
         echo '<li class="item">'.$accion_visible->boton."</li>";
--- template/nav/_redes_sociales.php
+++ template/nav/_redes_sociales.php
@@ -1,7 +1,12 @@
 <?php
-/** @var base\controller\controlador_base $controlador viene de links  */
+/**
+ * @var base\controller\controlador_base $controlador viene de links
+ * @var links_menu $links_menu
+ *
+ * */
 
 use gamboamartin\system\links_menu;
+
 
 ?>
 <div  class="top-bar color-primary">
@@ -13,6 +18,7 @@ use gamboamartin\system\links_menu;
         <?php
         if(isset($_SESSION['activa']) && (int)$_SESSION['activa'] === 1){ ?>
             <div class="pull-right col-md-12" style="text-align: right;">
+                <a  role="button" class="btn btn-info cerrar-session" href="<?php echo $links_menu->adm_session->inicio ?>"> Inicio </a>
                 <a role="button" class="btn btn-danger cerrar-session" href="<?php echo (new links_menu(link: $controlador->link,registro_id: $controlador->registro_id))->links->adm_session->logout ;?>"> Salir </a>
             </div>
         <?php }
