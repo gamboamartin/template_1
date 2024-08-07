0d8e485dce660a7318a8d84006142945799120ae
mgamboa <gamboa.martin@gmail.com>
2024-08-07 10:41:54 -0600

    v24.4.0 se integra item en br

--- template/head/items.php
+++ template/head/items.php
@@ -1,9 +1,13 @@
 <?php use config\views;
+/**
+ * @var base\controller\controlador_base $controlador
+ */
 use gamboamartin\system\links_menu; ?>
 <ul class="breadcrumb">
     <?php include (new views())->ruta_templates."breadcrumb/adm_session/inicio.php"; ?>
-    <?php include (new views())->ruta_templates."breadcrumb/alta.php"; ?>
-    <li class="item"><a role="button" class="btn btn-warning item-br" href="<?php  echo $controlador->link_lista; ?>"> Lista </a></li>
-    <li class="item"><a role="button" class="btn btn-warning item-br descarga_excel" href="<?php  echo $controlador->link_descarga_excel; ?>"> Descarga Excel </a></li>
-
+    <?php
+    foreach ($controlador->acciones_visibles_permitidas as $accion_visible) {
+        echo '<li class="item">'.$accion_visible->boton."</li>";
+    }
+    ?>
 </ul>
